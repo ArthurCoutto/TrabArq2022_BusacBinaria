630b9270e620b96e8e6d92394c42d22a26004775
ArthurCoutto <arthur.coutto@gmail.com>
2022-05-29 23:55:10 -0300

    Comentarios

--- BuscaBinaria.c
+++ BuscaBinaria.c
@@ -16,7 +16,7 @@ struct _Endereco
 	char lixo[2]; // Ao Espaço no final da linha + quebra de linha
 };
 
-int main(int argc, char**argv)
+int main(int argc, char**argv)  //Inicia funçao e recebe qtd de argumentos e criar array de argumentos 
 {
 	FILE *f;
 	Endereco e;
@@ -24,37 +24,38 @@ int main(int argc, char**argv)
 	int c,inicio,fim,tb,tr,meio;
 	char *aux;
 
-	if(argc != 2)
+	if(argc != 2) //Caso use o código errado, retorna erro
 	{
 		fprintf(stderr, "USO: %s [CEP]", argv[0]);
 		return 1;
 	}
 
 	c = 0;
-	printf("Tamanho da Estrutura: %ld\n\n", sizeof(Endereco));
+	printf("Tamanho da Estrutura: %ld\n\n", sizeof(Endereco)); //Indica tamanho do arquivo
 	f = fopen("cep_ordenado.dat","rb");
-    fseek(f,0,SEEK_END);
-    tb=ftell(f);
-    tr=tb/sizeof(Endereco);
+    fseek(f,0,SEEK_END); //Coloca a cabeça de leitura no fim do arquivo
+    tb=ftell(f);  //tb recebe a posiçao do ultimo byte do arquivo
+    tr=tb/sizeof(Endereco); //tr divide tb pelo tamanho do struct Endereço para achar o número total de endereços
     inicio=0;
-    fim=tr-1;
+    fim=tr-1; //Arrays em C começam em 0, então fim tem que ser a qtd de lotes-1 para funcionar
 	while(inicio <= fim)
 	{
-		c++;
+		c++; 
         meio=(inicio+fim)/2;
-        fseek(f,meio*sizeof(Endereco),SEEK_SET);
-        qt = fread(&e,sizeof(Endereco),1,f);
-		if(strncmp(argv[1],e.cep,8)==0){
+        fseek(f,meio*sizeof(Endereco),SEEK_SET); //Busca o endereço do meio no arquivo
+        qt = fread(&e,sizeof(Endereco),1,f); //Lê endereço do meio
+		if(strncmp(argv[1],e.cep,8)==0){ //Se encontrou, imprime informaços do CEP, numero de tentativas, fecha arquivo e finaliza funcao
 			printf("Cep encontrado!%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
             printf("Levou %d tentativas",c);
-            fclose(f);
+            fclose(f); 
             return 0;
-		}else if (strncmp(argv[1],e.cep,8)<0){
+		}else if (strncmp(argv[1],e.cep,8)<0){ //caso o CEP desejado seja menor que o meio, diminui fim e continua loop
             fim=meio-1;
-        }else{
+        }else{   //caso o CEP desejado seja maior que o meio, aumenta inicio e continua loop
             inicio=meio+1;
         };		
 	}
+	//Caso o CEP não seja encontrado, imprime a falha, quantas vezes tentou e fecha o arquivo
 	printf("Arquivo não encontrado \n");
 	printf("Levou %d tentativas\n",c);
 	fclose(f);
